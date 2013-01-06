164b34d508a06ac7801c0ea4da9fe25c994e7f1d
Anthony Langsworth <anthony.langsworth@gmail.com>
2013-01-07 01:31:04 +1100

    Finished last of the 10 critical effects.
    - Renamed GainBonusEffect to GainModifierEffect.
    - Added support for multiple scores and GainModifiers on Target.
    - Added defenses (Fort, Ref, Will and AC) to Your as well as AllDefenses.
    
    Future:
    - Need to add some more testing since only integration testing has been
      done on classes in the Effects namespaces.
    - Need to add critical effects to the origins and update the renderer to
      use them.

--- src/GammaWorldCharacter/Powers/Effects/Your.cs
+++ src/GammaWorldCharacter/Powers/Effects/Your.cs
@@ -42,5 +42,63 @@ namespace GammaWorldCharacter.Powers.Effects
                 return new CharacterScore(ScoreType.Speed);
             }
         }
+
+        /// <summary>
+        /// The character's Fortitude defense.
+        /// </summary>
+        public static CharacterScore Fortitude
+        {
+            get
+            {
+                return new CharacterScore(ScoreType.Fortitude);
+            }
+        }
+
+        /// <summary>
+        /// The character's Reflex defense.
+        /// </summary>
+        public static CharacterScore Reflex
+        {
+            get
+            {
+                return new CharacterScore(ScoreType.Reflex);
+            }
+        }
+
+        /// <summary>
+        /// The character's Will defense.
+        /// </summary>
+        public static CharacterScore Will
+        {
+            get
+            {
+                return new CharacterScore(ScoreType.Will);
+            }
+        }
+
+        /// <summary>
+        /// The character's Armor Class defense.
+        /// </summary>
+        public static CharacterScore ArmorClass
+        {
+            get
+            {
+                return new CharacterScore(ScoreType.ArmorClass);
+            }
+        }
+
+        /// <summary>
+        /// All defenses (that is Fort, Ref, Will and AC).
+        /// </summary>
+        public static IEnumerable<CharacterScore> Defenses
+        {
+            get
+            {
+                yield return new CharacterScore(ScoreType.Fortitude);
+                yield return new CharacterScore(ScoreType.Reflex);
+                yield return new CharacterScore(ScoreType.Will);
+                yield return new CharacterScore(ScoreType.ArmorClass);
+            }
+        }
     }
 }
