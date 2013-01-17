b945a0b32f491a037e3acd5ac5068ddc672068b5
Anthony Langsworth <anthony.langsworth@gmail.com>
2013-01-18 00:15:31 +1100

    Started working on JSON serialization.
    - Created GammaWorldCharacter.Serialization and added Newtonsoft.Json.
    - Started defining a JSON schema.

---
+++ GammaWorldCharacter.Serialization/JzonSerializer.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Newtonsoft.Json;
+using System.Resources;
+
+namespace GammaWorldCharacter.Serialization
+{
+    /// <summary>
+    /// A serializer
+    /// </summary>
+    public class JzonSerializer
+    {
+        /// <summary>
+        /// Serialize the given <see cref="Character"/> to JSON.
+        /// </summary>
+        /// <param name="character">
+        /// The <see cref="Character"/> to serialize. This cannot be null.
+        /// </param>
+        /// <returns>
+        /// The character as JSON.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="character"/> cannot be null.
+        /// </exception>
+        public string Serialize(Character character)
+        {
+            if (character == null)
+            {
+                throw new ArgumentNullException("character");
+            }
+
+            CharacterJsonData characterJsonData;
+
+            characterJsonData = new CharacterJsonData();
+            characterJsonData.Name = character.Name;
+            characterJsonData.PlayerName = character.PlayerName;
+            foreach (ScoreType abilityScore in ScoreTypeHelper.AbilityScores)
+            {
+                characterJsonData.AbilityScores[ScoreTypeHelper.ToString(abilityScore)] = 
+                    character[abilityScore].Total;
+            }
+            characterJsonData.PrimaryOrigin = character.PrimaryOrigin.GetType().FullName;
+            characterJsonData.SecondaryOrigin = character.SecondaryOrigin.GetType().FullName;
+
+            return JsonConvert.SerializeObject(characterJsonData);
+        }
+
+        /// <summary>
+        /// The JSON Schema describing the serialized characters.
+        /// </summary>
+        public string Schema
+        {
+            get
+            {
+                // TODO: Cache this for efficiency
+                using (Stream stream = 
+                    Assembly.GetCallingAssembly().GetManifestResourceStream(
+                        "GammaWorldCharacter.Serialization.Schema.json"))
+                using (StreamReader streamReader = new StreamReader(stream))
+                {
+                    return streamReader.ReadToEnd();
+                }
+            }
+        }
+    }
+}
---
+++ src/GammaWorldCharacter.Test.Unit/TestSerialization.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace GammaWorldCharacter.Test.Unit
+{
+    [TestFixture]
+    public class TestSerialization
+    {
+    }
+}
