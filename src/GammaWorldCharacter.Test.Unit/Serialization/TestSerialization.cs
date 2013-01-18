317eaf6b80f9f98687425a5acbe844ebdd3686d0
Anthony Langsworth <anthony.langsworth@gmail.com>
2013-01-18 15:53:29 +1100

    Debugging JSON schema. Committing as a backup.

---
+++ src/GammaWorldCharacter.Test.Unit/Serialization/TestSerialization.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GammaWorldCharacter.Samples;
+using GammaWorldCharacter.Serialization;
+using NUnit.Framework;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
+
+namespace GammaWorldCharacter.Test.Unit.Serialization
+{
+    [TestFixture]
+    public class TestSerialization
+    {
+        [Test]
+        public void TestJsonSerialization()
+        {
+            JsonCharacterSerializer characterSerializer;
+
+            characterSerializer = new JsonCharacterSerializer();
+            string json = characterSerializer.Serialize(Level01Characters.Clip);
+
+            JsonSchema jsonSchema;
+
+            jsonSchema = JsonSchema.Parse(characterSerializer.Schema);
+            JObject jObject;
+
+            IList<string> messages;
+            
+            jObject = JObject.Parse(json);
+            jObject.IsValid(jsonSchema, out messages);
+
+            Assert.That(messages, Is.EquivalentTo(new string[0]));
+        }
+    }
+}
---
+++ src/GammaWorldCharacter/Serialization/CharacterJsonData.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace GammaWorldCharacter.Serialization
+{
+    /// <summary>
+    /// A class that stores the details of a <see cref="Character"/> for 
+    /// serialization and deserialization.
+    /// </summary>
+    public class CharacterJsonData
+    {
+        /// <summary>
+        /// Create a new <see cref="CharacterJsonData"/>.
+        /// </summary>
+        public CharacterJsonData()
+        {
+            AbilityScores = new Dictionary<string, int>();
+        }
+
+        /// <summary>
+        /// The character name.
+        /// </summary>
+        [JsonProperty("name")]
+        public string Name;
+
+        /// <summary>
+        /// The name of the player that uses the character.
+        /// </summary>
+        [JsonProperty("playerName")]
+        public string PlayerName;
+
+        /// <summary>
+        /// The ability scores (Strength, Constitution, Dexterity, Intelligence, Wisdom, Charisma).
+        /// </summary>
+        [JsonProperty("abilityScores")]
+        public Dictionary<string, int> AbilityScores
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// The name of the primary origin <see cref="Type"/>.
+        /// </summary>
+        [JsonProperty("primaryOrigin")]
+        public string PrimaryOrigin;
+
+        /// <summary>
+        /// The name of the secondary origin <see cref="Type"/>.
+        /// </summary>
+        [JsonProperty("secondaryOrigin")]
+        public string SecondaryOrigin;
+
+        // TODO: Gear (held items, equipped items, carried items), trained skill, levels
+    }
+}
